1c888a95e95301b09ed2a8e697aef744f195848c
aa <aa@m.ru>
2024-08-08 23:04:50 +0200

    x

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(vit497.ru)
+domains=(ovsyank.ru)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="petro496@gmail.com" # Adding a valid address is strongly recommended
